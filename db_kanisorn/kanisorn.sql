c426bbf0ad2e7b18c84e5bdc73a55e59a725d527
Chaiwat <mynameistecs51@gmail.com>
2015-09-06 23:42:38 +0700

    chaiwat update database

--- db_kanisorn/kanisorn.sql
+++ db_kanisorn/kanisorn.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 06, 2015 at 06:23 PM
--- Server version: 5.6.24
--- PHP Version: 5.5.24
+-- Host: localhost
+-- Generation Time: Sep 06, 2015 at 11:41 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.5.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `file_document` (
-  `file_docId` int(11) NOT NULL,
+`file_docId` int(11) NOT NULL,
   `file_subName` text NOT NULL,
   `file_docPath` text NOT NULL,
   `file_docDetail` text NOT NULL
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `file_document` (
 
 INSERT INTO `file_document` (`file_docId`, `file_subName`, `file_docPath`, `file_docDetail`) VALUES
 (0, 'test update', '17_08_15_16_48_11document.pdf', 'ทดสอบการอัพเดท'),
-(3, 'dd', '28_08_15_14_13_452.png', 'dd');
+(3, 'dd', '06_09_15_22_01_47_.jpg', 'dd');
 
 -- --------------------------------------------------------
 
@@ -48,13 +48,13 @@ INSERT INTO `file_document` (`file_docId`, `file_subName`, `file_docPath`, `file
 --
 
 CREATE TABLE IF NOT EXISTS `research` (
-  `res_id` int(11) NOT NULL,
+`res_id` int(11) NOT NULL,
   `res_name` text NOT NULL,
   `res_file` text NOT NULL,
   `res_pict` text NOT NULL,
   `res_detail` text NOT NULL,
   `res_type` enum('national','international') NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `research`
@@ -63,7 +63,9 @@ CREATE TABLE IF NOT EXISTS `research` (
 INSERT INTO `research` (`res_id`, `res_name`, `res_file`, `res_pict`, `res_detail`, `res_type`) VALUES
 (14, 'national', '06_09_15_17_37.docx', '06_09_15_17_37_.png,06_09_15_17_37_1.png', 'national', 'national'),
 (15, 'international', '06_09_15_17_371.docx', '06_09_15_17_37_2.png,06_09_15_17_37_3.png', 'international', 'international'),
-(23, 'national test', '06_09_15_18_05.docx', '06_09_15_18_05_.png,06_09_15_18_05_1.png', 'national test', 'national');
+(24, 'national test', '06_09_15_21_14.docx', '06_09_15_21_27_.jpg,06_09_15_21_27_1.jpg', 'national test', 'national'),
+(25, 'national1', '06_09_15_22_30_.PDF', '06_09_15_22_30_.jpg,06_09_15_22_30_1.jpg', 'national1', 'national'),
+(26, 'international test', '06_09_15_22_36_.PDF', '06_09_15_22_36_.jpg,06_09_15_22_36_1.jpg', 'international', 'international');
 
 -- --------------------------------------------------------
 
@@ -73,6 +75,7 @@ INSERT INTO `research` (`res_id`, `res_name`, `res_file`, `res_pict`, `res_detai
 
 CREATE TABLE IF NOT EXISTS `table_teacher` (
   `table_id` int(11) NOT NULL DEFAULT '0',
+  `table_trem` char(9) NOT NULL,
   `table_name` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -80,8 +83,8 @@ CREATE TABLE IF NOT EXISTS `table_teacher` (
 -- Dumping data for table `table_teacher`
 --
 
-INSERT INTO `table_teacher` (`table_id`, `table_name`) VALUES
-(0, '16-8-255813-31-10.jpg');
+INSERT INTO `table_teacher` (`table_id`, `table_trem`, `table_name`) VALUES
+(0, '1/2558', '06_09_15_23_21_23_.jpg');
 
 -- --------------------------------------------------------
 
@@ -90,7 +93,7 @@ INSERT INTO `table_teacher` (`table_id`, `table_name`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `university` (
-  `uni_id` int(11) NOT NULL,
+`uni_id` int(11) NOT NULL,
   `uni_data` text NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
@@ -111,25 +114,25 @@ INSERT INTO `university` (`uni_id`, `uni_data`) VALUES
 -- Indexes for table `file_document`
 --
 ALTER TABLE `file_document`
-  ADD PRIMARY KEY (`file_docId`);
+ ADD PRIMARY KEY (`file_docId`);
 
 --
 -- Indexes for table `research`
 --
 ALTER TABLE `research`
-  ADD PRIMARY KEY (`res_id`);
+ ADD PRIMARY KEY (`res_id`);
 
 --
 -- Indexes for table `table_teacher`
 --
 ALTER TABLE `table_teacher`
-  ADD PRIMARY KEY (`table_id`);
+ ADD PRIMARY KEY (`table_trem`);
 
 --
 -- Indexes for table `university`
 --
 ALTER TABLE `university`
-  ADD PRIMARY KEY (`uni_id`);
+ ADD PRIMARY KEY (`uni_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -139,17 +142,17 @@ ALTER TABLE `university`
 -- AUTO_INCREMENT for table `file_document`
 --
 ALTER TABLE `file_document`
-  MODIFY `file_docId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `file_docId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `research`
 --
 ALTER TABLE `research`
-  MODIFY `res_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
+MODIFY `res_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT for table `university`
 --
 ALTER TABLE `university`
-  MODIFY `uni_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `uni_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
